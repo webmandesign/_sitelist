c244b7f1673086cc9a4a900b36222efb25d152c8
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2023-01-11 01:23:29 +0100

    1.6.0

--- functions.php
+++ functions.php
@@ -6,243 +6,170 @@
  * @copyright  WebMan Design, Oliver Juhas
  *
  * @since    1.0.0
- * @version  1.5.0
- *
- * Contents:
- *
- *  0) Init
- * 10) Setup
- * 20) List of sites
+ * @version  1.6.0
  */
 class SiteList {
 
+	/**
+	 * Initialization.
+	 *
+	 * @since    1.0.0
+	 * @version  1.6.0
+	 */
+	public static function init() {
 
+		// Processing
 
+			// Hooks
 
+				// Actions
+
+					add_action( 'after_setup_theme', __CLASS__ . '::setup' );
+
+					add_action( 'wp_enqueue_scripts', __CLASS__ . '::assets' );
+
+					add_action( 'tha_html_before', __CLASS__ . '::doctype' );
+
+					add_action( 'wp_head', __CLASS__ . '::head', 0 );
+
+					add_action( 'tha_content_top', __CLASS__ . '::list' );
+
+	} // /init
 
 	/**
-	 * 0) Init
+	 * Theme setup.
+	 *
+	 * @since    1.0.0
+	 * @version  1.6.0
 	 */
+	public static function setup() {
 
-		private static $instance;
+		// Processing
 
+			add_theme_support( 'title-tag' );
 
+			add_theme_support( 'html5', array(
+				'caption',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'navigation-widgets',
+				'search-form',
+				'script',
+				'style',
+			) );
 
-		/**
-		 * Constructor
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		private function __construct() {
-
-			// Processing
-
-				// Hooks
-
-					// Actions
-
-						add_action( 'after_setup_theme', __CLASS__ . '::setup' );
-
-						add_action( 'wp_enqueue_scripts', __CLASS__ . '::assets' );
-
-						add_action( 'tha_html_before', __CLASS__ . '::doctype' );
-
-						add_action( 'wp_head', __CLASS__ . '::head', 1 );
-
-						add_action( 'tha_content_top', __CLASS__ . '::list' );
-
-		} // /__construct
-
-
-
-		/**
-		 * Initialization (get instance)
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		public static function init() {
-
-			// Processing
-
-				if ( null === self::$instance ) {
-					self::$instance = new self;
-				}
-
-
-			// Output
-
-				return self::$instance;
-
-		} // /init
-
-
-
-
+	} // /setup
 
 	/**
-	 * 10) Setup
+	 * HTML doctype.
+	 *
+	 * @since    1.0.0
+	 * @version  1.0.0
 	 */
+	public static function doctype() {
 
-		/**
-		 * Theme setup
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		public static function setup() {
+		// Output
 
-			// Processing
+			echo '<!DOCTYPE html>';
 
-				// Title tag
-
-					/**
-					 * @link  https://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
-					 */
-					add_theme_support( 'title-tag' );
-
-				// Enable HTML5 markup
-
-					/**
-					 * @link  https://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-					 */
-					add_theme_support( 'html5', array(
-							'caption',
-							'comment-form',
-							'comment-list',
-							'gallery',
-							'search-form',
-						) );
-
-		} // /setup
-
-
-
-		/**
-		 * HTML DOCTYPE
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		public static function doctype() {
-
-			// Output
-
-				echo '<!DOCTYPE html>';
-
-		} // /doctype
-
-
-
-		/**
-		 * HTML HEAD
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		public static function head() {
-
-			// Helper variables
-
-				$output = array();
-
-
-			// Processing
-
-				$output[10] = '<meta charset="' . get_bloginfo( 'charset' ) . '">';
-				$output[20] = '<meta name="viewport" content="width=device-width, initial-scale=1">';
-				$output[30] = '<link rel="profile" href="http://gmpg.org/xfn/11">';
-
-
-			// Output
-
-				echo implode( "\r\n", $output ) . "\r\n";
-
-		} // /head
-
-
-
-		/**
-		 * Assets
-		 *
-		 * @since    1.0.0
-		 * @version  1.0.0
-		 */
-		public static function assets() {
-
-			// Processing
-
-				wp_enqueue_style( '_sitelist_styles', get_stylesheet_uri() );
-
-		} // /assets
-
-
-
-
+	} // /doctype
 
 	/**
-	 * 20) List of sites
+	 * HTML head.
+	 *
+	 * @since    1.0.0
+	 * @version  1.0.0
 	 */
+	public static function head() {
 
-		/**
-		 * List of sites
-		 *
-		 * @since    1.0.0
-		 * @version  1.5.0
-		 */
-		public static function list() {
+		// Variables
 
-			// Helper variables
-
-				$output = array();
-
-				$sites = get_sites( array(
-					'site__not_in' => array( 1 ),
-					'public'       => 1,
-					'order'        => 'DESC',
-				) );
+			$output = array();
 
 
-			// Processing
+		// Processing
 
-				foreach ( $sites as $site ) {
-
-					$site = get_blog_details( $site->blog_id );
-
-					$t = wp_get_theme( get_blog_option( $site->blog_id, 'stylesheet' ) );
-
-					$theme['name']        = $t->get( 'Name' );
-					$theme['description'] = $t->get( 'Description' );
-					$theme['screenshot']  = $t->get_screenshot();
-					$theme['url']         = $t->get( 'ThemeURI' );
-
-					$output_single = '';
-
-					$output_single .= '<a href="' . esc_url( $site->siteurl ) . '">';
-					$output_single .= '<div class="site-image"><img src="' . esc_url( $theme['screenshot'] ) . '" alt="' . esc_attr( $theme['name'] ) . ' Theme Demo Website" /></div>';
-					$output_single .= '<h2 class="site-title" title="' . esc_attr( $theme['name'] ) . ' Theme Demo Website">' . $site->blogname . ' <small>' . $theme['name'] . ' Theme Demo</small></h2>';
-					$output_single .= '</a>';
-
-					$output[] = $output_single;
-
-				} // /foreach
+			$output[10] = '<meta charset="' . get_bloginfo( 'charset' ) . '">';
+			$output[20] = '<meta name="viewport" content="width=device-width, initial-scale=1">';
+			$output[30] = '<link rel="profile" href="http://gmpg.org/xfn/11">';
 
 
-			// Output
+		// Output
 
-				if ( ! empty( $output ) ) {
-					echo '<h1>Themes by <a href="http://www.webmandesign.eu">WebMan Design</a></h1>';
-					echo '<ul class="sites"><li class="site">' . implode( '</li><li class="site">', $output ) . '</li></ul>';
-				}
+			echo implode( "\r\n", $output ) . "\r\n";
+
+	} // /head
+
+	/**
+	 * Assets.
+	 *
+	 * @since    1.0.0
+	 * @version  1.0.0
+	 */
+	public static function assets() {
+
+		// Processing
+
+			wp_enqueue_style( '_sitelist_styles', get_stylesheet_uri() );
+
+	} // /assets
+
+	/**
+	 * List of sites.
+	 *
+	 * @since    1.0.0
+	 * @version  1.6.0
+	 */
+	public static function list() {
+
+		// Variables
+
+			$output = array();
+
+			$sites = get_sites( array(
+				'site__not_in' => array( 1 ),
+				'public'       => 1,
+				'order'        => 'DESC',
+			) );
 
 
-		} // /list
+		// Processing
+
+			foreach ( $sites as $site ) {
+
+				$site = get_blog_details( $site->blog_id );
+
+				$t = wp_get_theme( get_blog_option( $site->blog_id, 'stylesheet' ) );
+
+				$class  = 'theme--' . sanitize_title( $t->get_template() );
+				$class .= ( $site->mature ) ? ( ' is-alt is-mature' ) : ( '' );
+
+				$theme['name']        = $t->get( 'Name' );
+				$theme['description'] = $t->get( 'Description' );
+				$theme['screenshot']  = $t->get_screenshot();
+				$theme['url']         = $t->get( 'ThemeURI' );
+
+				$output_single  = '<li class="site ' . esc_attr( $class ) . '">';
+				$output_single .= '<a href="' . esc_url( $site->siteurl ) . '">';
+				$output_single .= '<div class="site-image"><img src="' . esc_url( $theme['screenshot'] ) . '" alt="' . esc_attr( $theme['name'] ) . ' Theme Demo Website" /></div>';
+				$output_single .= '<h2 class="site-title" title="' . esc_attr( $theme['name'] ) . ' Theme Demo Website">' . $site->blogname . ' <small>' . $theme['name'] . ' Theme Demo</small></h2>';
+				$output_single .= '</a>';
+				$output_single .= '</li>';
+
+				$output[] = $output_single;
+			}
 
 
+		// Output
 
+			if ( ! empty( $output ) ) {
+				echo '<h1>Themes by <a href="http://www.webmandesign.eu">WebMan Design</a></h1>';
+				echo '<ul class="sites">' . PHP_EOL . implode( PHP_EOL, $output ) . PHP_EOL . '</ul>';
+			}
 
+	} // /list
 
-} // /SiteList
+}
 
 add_action( 'after_setup_theme', 'SiteList::init', -10 );
